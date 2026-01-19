4f205553fe24a854074c705da35b2638b24620aa
Astrasv <sudharsan.vanamali@gmail.com>
2026-01-19 15:30:44 +0530

    Voice roasts and chrome extenstion integration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,39 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+
+from utils.roaster_speaker import speak_alert
+from data.forbidden import FORBIDDEN
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return "<p>Under dev</p>"
+CORS(app) 
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+
+@app.route('/update', methods=['POST'])
+def receive_url():
+    data = request.json
+    url = data.get('url', '')
+    
+    print(f" ==> Active: {url}")
+
+    for keyword in FORBIDDEN:
+        if keyword in url:
+            print(f"!!! VIOLATION: {keyword} !!!")
+            speak_alert("Don't open please")
+            
+    return "OK", 200
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return "Activity Spy is running.", 200
+
+
+if __name__ == "__main__":
+
+    print("--- Activity Spy Running (CORS Enabled) ---")
+    app.run(port=5000)
